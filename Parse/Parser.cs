161608300ab86820c561225300e46435a81f6c2b
HannaCunningham <hcunni6@lsu.edu>
2016-10-06 16:24:55 -0500

    We think the parser works now
    Hooray! (Maybe)

---
+++ Parse/Parser.cs
@@ -0,0 +1,144 @@
+// Parser -- the parser for the Scheme printer and interpreter
+//
+// Defines
+//
+//   class Parser;
+//
+// Parses the language
+//
+//   exp  ->  ( rest
+//         |  #f
+//         |  #t
+//         |  ' exp
+//         |  integer_constant
+//         |  string_constant
+//         |  identifier
+//    rest -> )
+//         |  exp+ [. exp] )
+//
+// and builds a parse tree.  Lists of the form (rest) are further
+// `parsed' into regular lists and special forms in the constructor
+// for the parse tree node class Cons.  See Cons.parseList() for
+// more information.
+//
+// The parser is implemented as an LL(0) recursive descent parser.
+// I.e., parseExp() expects that the first token of an exp has not
+// been read yet.  If parseRest() reads the first token of an exp
+// before calling parseExp(), that token must be put back so that
+// it can be reread by parseExp() or an alternative version of
+// parseExp() must be called.
+//
+// If EOF is reached (i.e., if the scanner returns a NULL) token,
+// the parser returns a NULL tree.  In case of a parse error, the
+// parser discards the offending token (which probably was a DOT
+// or an RPAREN) and attempts to continue parsing with the next token.
+
+using System;
+using Tokens;
+using Tree;
+
+namespace Parse
+{
+    public class Parser
+    {
+	
+        private Scanner scanner;
+
+        public Parser(Scanner s) { scanner = s; }
+
+        public Node parseExp()
+        {
+            Token tok = scanner.getNextToken();
+            return parseExp(tok);
+        }
+
+        public Node parseExp(Token tok)
+        {
+            // TODO: write code for parsing an exp
+
+            //edit
+            if(tok == null)
+            {
+                return new Node();
+            }
+            else if(tok.getType().ToString().Equals("LPAREN"))
+            {
+                return parseRest();
+            }
+            else if (tok.getType().ToString().Equals("FALSE"))
+            {
+                return new BoolLit(false);
+            }
+            else if (tok.getType().ToString().Equals("TRUE"))
+            {
+                return new BoolLit(true);
+            }
+            else if (tok.getType().ToString().Equals("QUOTE"))
+            {
+                return new Cons(new Ident("quote"), new Cons(parseExp(), new Nil()));
+            }
+            else if (tok.getType().ToString().Equals("INT"))
+            {
+                return new IntLit(tok.getIntVal());
+            }
+            else if (tok.getType().ToString().Equals("STRING"))
+            {
+                return new StringLit(tok.getStringVal());
+            }
+            else if (tok.getType().ToString().Equals("IDENT"))
+            {
+                return new Ident(tok.getName());
+            }
+            else
+            {
+                Console.WriteLine("Syntax error");
+            }
+            //end edit
+
+            return null;
+        }
+
+        protected Node parseRest()
+        {
+            Token tok = scanner.getNextToken();
+            return parseRest(tok);
+        }
+
+
+        protected Node parseRest(Token tok)
+        {
+            // TODO: write code for parsing a rest
+
+            //edit
+            if(tok.getType().ToString().Equals("RPAREN"))
+            {
+                return new Nil();
+            } else if (!tok.getType().ToString().Equals("DOT"))
+            {
+                Node a = parseExp(tok);
+                Node d;
+                Token lookahead = scanner.getNextToken();
+                if (lookahead.getType().ToString().Equals("DOT"))
+                {
+                    d = new Cons(new Ident("Dot"), new Cons(parseExp(), new Nil()));
+                } else
+                {
+                    d = parseRest(lookahead);
+                }
+
+                return new Cons(a, d);
+            }
+            else
+            {
+                // error handling
+                Console.WriteLine("Incorrect syntax");
+            }
+            //end edit
+
+            return null;
+        }
+
+        // TODO: Add any additional methods you might need.
+    }
+}
+
---
+++ Parse/Scanner.cs
@@ -0,0 +1,174 @@
+// Scanner -- The lexical analyzer for the Scheme printer and interpreter
+
+using System;
+using System.IO;
+using Tokens;
+
+namespace Parse
+{
+    public class Scanner
+    {
+        private TextReader In;
+
+        // maximum length of strings and identifier
+        private const int BUFSIZE = 1000;
+        private char[] buf;
+
+        public Scanner(TextReader i) { In = i; }
+  
+        // TODO: Add any other methods you need
+
+        public Token getNextToken()
+        {
+            int ch;
+            buf =  new char[BUFSIZE];
+            try
+            {
+                // It would be more efficient if we'd maintain our own
+                // input buffer and read characters out of that
+                // buffer, but reading individual characters from the
+                // input stream is easier.
+                ch = In.Read();
+   
+                // TODO: skip white space and comments
+
+                if (ch == -1)
+                    return null;
+        
+                // Special characters
+                else if (ch == '\'')
+                    return new Token(TokenType.QUOTE);
+                else if (ch == '(')
+                    return new Token(TokenType.LPAREN);
+                else if (ch == ')')
+                    return new Token(TokenType.RPAREN);
+                else if (ch == '.')
+                    // We ignore the special identifier `...'.
+                    return new Token(TokenType.DOT);
+                
+                // Boolean constants
+                else if (ch == '#')
+                {
+                    ch = In.Read();
+
+                    if (ch == 't')
+                        return new Token(TokenType.TRUE);
+                    else if (ch == 'f')
+                        return new Token(TokenType.FALSE);
+                    else if (ch == -1)
+                    {
+                        Console.Error.WriteLine("Unexpected EOF following #");
+                        return null;
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine("Illegal character '" +
+                                                (char)ch + "' following #");
+                        return getNextToken();
+                    }
+                }
+
+                // String constants
+                else if (ch == '"')
+                {
+                    // TODO: scan a string into the buffer variable buf
+
+                    //edit
+                    int i = 0;
+                    while(In.Peek() != '"')
+                    {
+                        buf[i] = (char) In.Read();
+                        i++;
+                    }
+                    String str = new string(buf, 0, i + 1);
+                    str = str.Replace(Convert.ToChar(0x0).ToString(), "");
+
+                    return new StringToken(str);
+                    //end edit
+
+                }
+
+    
+                // Integer constants
+                else if (ch >= '0' && ch <= '9')
+                {
+                    Console.WriteLine("There's a number!");
+                    int i = 0;
+                    // TODO: scan the number and convert it to an integer
+                    char s = Convert.ToChar(ch);
+
+                    //edit
+                    String str = System.String.Empty;
+                    str += s;
+                    while(In.Peek() >= '0' && In.Peek() <= '9')
+                    {
+                        char t = Convert.ToChar(In.Read());
+                        str += t;
+                    }
+                    i = Convert.ToInt32(str);
+                    //end edit
+
+                    // make sure that the character following the integer
+                    // is not removed from the input stream
+                    return new IntToken(i);
+                }
+        
+                // Identifiers
+                else if (ch >= 'A' && ch <= 'z'
+                         // or ch is some other valid first character
+                         // for an identifier
+                         ) {
+                    // TODO: scan an identifier into the buffer
+
+                    //edit
+                    buf[0] = (char) ch;
+                    int i = 1;
+                    while (In.Peek() != ' ')
+                    {
+                        buf[i] = (char)In.Read();
+                        i++;
+                    }
+                    // make sure that the character following the integer
+                    // is not removed from the input stream
+
+                    return new IdentToken(new String(buf, 0, i+1));
+                    //end edit 
+
+                }
+
+                //edit to skip comments
+                else if(ch == ';')
+                {
+                    while(In.Peek() != '\n')
+                    {
+                        In.Read();
+                    }
+                    In.Read();
+                    return getNextToken();
+                }
+                //skip white space, tab, newline, etc
+                else if(ch == ' ' || ch == '\f' || ch == '\t' || ch =='\v' || ch =='\r' || ch == '\n')
+                {
+                    return getNextToken();
+                }
+                //end edit
+    
+                // Illegal character
+                else
+                {
+                    Console.Error.WriteLine("Illegal input character '"
+                                            + (char)ch + '\'');
+                    return getNextToken();
+                }
+            }
+            catch (IOException e)
+            {
+                Console.Error.WriteLine("IOException: " + e.Message);
+                return null;
+            }
+        } //end getNextToken
+        
+    }
+
+}
+
