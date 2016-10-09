7690ca1d8e7461a3e9fa731acaa6b692d17c9c31
foxfire13795 <bma1194@yahoo.com>
2016-10-08 23:57:52 -0500

    Fixed if formatting

--- PrettyPrinter/PrettyPrinter/Special/If.cs
+++ PrettyPrinter/PrettyPrinter/Special/If.cs
@@ -30,16 +30,15 @@ namespace Tree
                 {
                     if(t.getCar().isPair())
                     {
-                        t.getCar().print(n, false);
+                        t.getCar().print(0 - n, false);
                         Console.WriteLine();
                     }else
                     {
                         t.getCar().print(n, p);
                         Console.WriteLine();
                     }
-
-                    t.getCdr().print(n, true);
-                    Console.WriteLine();
+                    
+                    printHelper(t.getCdr(), n);
 
                 } else
                 {
@@ -48,124 +47,24 @@ namespace Tree
                 }
             }
         }
-        //end edit
 
-        //WOAH WOAH WOAH
-
-        /*public override void print(Node t, int n, bool p)
+        public void printHelper(Node t, int n)
         {
-            if (p)
-            {
-                t.getCar().print(-n, false);
-                Console.WriteLine();
-                printIf(t.getCdr(), n);
-            }
-            else
-            {
-                if (n > 0)
-                {
-                    for (int i = 0; i < n; i++)
-                        Console.Write(" ");
-                }
-                Console.Write("(if ");
-            }
-        }
-        private void printIf(Node t, int n)
-        {
-            if (t.getCdr().isNull())
+            if(t.getCdr().isNull())
             {
                 t.getCar().print(n + 4, false);
                 Console.WriteLine();
-                if (n > 0)
-                {
-                    for (int i = 0; i < n; i++)
-                        Console.Write(" ");
-                }
+                t.indent(n);
                 t.getCdr().print(n + 4, true);
-                return;
+                Console.WriteLine();
             }
-            t.getCar().print(n + 4, false);
-            Console.WriteLine();
-            printIf(t.getCdr(), n);
-        }*/
-
-        //end woah
-
-        //public override void print(Node t, int n, bool p)
-        //{
-        // TODO: Implement this function.
-
-        //edit
-        //Console.Write("(if");
-        //if...
-        //end edit
-        //}
+            else
+            {
+                t.getCar().print(n + 4, false);
+                Console.WriteLine();
+                printHelper(t.getCdr(), n);
+            }
+        }
     }
 }
-//// If -- Parse tree node strategy for printing the special form if
-
-//using System;
-
-//namespace Tree
-//{
-//    public class If : Special
-//    {
-//        // TODO: Add any fields needed.
- 
-//        // TODO: Add an appropriate constructor.
-//	public If() { }
-
-//        //WOAH WOAH WOAH
-
-//        public override void print(Node t, int n, bool p)
-//        {
-//            if (p)
-//            {
-//                t.getCar().print(-n, false);
-//                Console.WriteLine();
-//                printIf(t.getCdr(), n);
-//            }
-//            else
-//            {
-//                if (n > 0)
-//                {
-//                    for (int i = 0; i < n; i++)
-//                        Console.Write(" ");
-//                }
-//                Console.Write("(if ");
-//            }
-//        }
-
-//        private void printIf(Node t, int n)
-//        {
-//            if (t.getCdr().isNull())
-//            {
-//                t.getCar().print(n + 4, false);
-//                Console.WriteLine();
-//                if (n > 0)
-//                {
-//                    for (int i = 0; i < n; i++)
-//                        Console.Write(" ");
-//                }
-//                t.getCdr().print(n + 4, true);
-//                return;
-//            }
-//            t.getCar().print(n + 4, false);
-//            Console.WriteLine();
-//            printIf(t.getCdr(), n);
-//        }
-
-//        //end woah
-
-//        //public override void print(Node t, int n, bool p)
-//        //{
-//        // TODO: Implement this function.
-
-//        //edit
-//        //Console.Write("(if");
-//        //if...
-//        //end edit
-//        //}
-//    }
-//}
 
