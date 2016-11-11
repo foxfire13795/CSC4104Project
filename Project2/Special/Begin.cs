79c48cdf702bf647d873c22cebd7797cb9be9e5d
HannaCunningham <hcunni6@lsu.edu>
2016-11-10 19:00:19 -0600

    added source files for prog2
    -eval method added for Begin (might be wrong)
    -eval method added for Cond
    -evalbody added on Cons
    -evallist exists but needs implementing in Cons

---
+++ Project2/Special/Begin.cs
@@ -0,0 +1,21 @@
+// Begin -- Parse tree node strategy for printing the special form begin
+
+using System;
+
+namespace Tree
+{
+    public class Begin : Special
+    {
+	public Begin() { }
+
+        public override void print(Node t, int n, bool p)
+        {
+            Printer.printBegin(t, n, p);
+        }
+        public Node eval(Node node, Environment env)
+        {
+            return Nil.getInstance();
+        }
+    }
+}
+
---
+++ Project2/Special/Cond.cs
@@ -0,0 +1,23 @@
+// Cond -- Parse tree node strategy for printing the special form cond
+
+using System;
+
+namespace Tree
+{
+    public class Cond : Special
+    {
+	public Cond() { }
+
+        public override void print(Node t, int n, bool p)
+        { 
+            Printer.printCond(t, n, p);
+        }
+
+        public Node eval(Node node, Environment env)
+        {
+
+        }
+    }
+}
+
+
