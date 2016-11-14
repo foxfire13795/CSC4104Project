9c1f0cc24013f6186b67214c5ed9c1c62b4d85bf
HannaCunningham <hcunni6@lsu.edu>
2016-11-13 18:06:26 -0600

    let works

--- Project2/Special/Let.cs
+++ Project2/Special/Let.cs
@@ -15,15 +15,48 @@ namespace Tree
 
         //spring
         //from scheme function
+        //public Environment letEnv(Node exp, Environment env)
+        //{
+            //env.define(exp.getCdr().getCar().getCar(), eval(exp.getCdr().getCar().getCdr().getCar(), env)); //not sure if should be define or assign?
+            //return env;
+        //}
+
         public Environment letEnv(Node exp, Environment env)
         {
-            env.define(exp.getCar().getCar().getCdr(), eval(exp.getCar().getCdr().getCar().getCdr(), env)); //not sure if should be define or assign?
+            Node defName;
+            Node defValue;
+            if(exp.getCar().isPair())
+            {
+                defName = exp.getCar().getCar();
+                defValue = exp.getCar().getCdr().getCar();
+                env.define(defName, defValue);
+            }
+            if(!exp.getCdr().isNull() && exp.getCdr().isPair())
+            {
+                if(!exp.getCdr().getCdr().isNull())
+                {
+                    env = letEnv(exp.getCdr(), env);
+                }
+                else
+                {
+                    defName = exp.getCdr().getCar().getCar();
+                    defValue = exp.getCdr().getCar().getCdr().getCar();
+                    env.define(defName, defValue);
+                }
+            }
             return env;
         }
 
+
         public override Node eval(Node exp, Environment env)
         {
-            return eval(exp.getCdr().getCdr(), letEnv(exp, env));
+            Node def = exp.getCdr().getCar();
+            if(def.isPair())
+            {
+                env = letEnv(def, env);
+            }
+            return exp.getCdr().getCdr().eval(env);
+            //return eval(exp.getCdr().getCdr(), letEnv(exp, env));
         }
 
     }
